2ee822415c5cc60236b0fb30664e7773d826e14a
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-13 22:29:10 -0300

    Importing the component into main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Layout from "./Components/Layout";
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
