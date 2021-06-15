b4bb6068fc0ed4d665bb1968b5811fcf6f3553ce
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-15 18:30:52 -0300

    Applying grid layout rule

--- src/Components/Layout.vue
+++ src/Components/Layout.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Teste</div>
+  <div id="grid">
+    <h2>Eu sou um titulo</h2>
+  </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang="scss">
 
+<style scoped>
+  #grid {
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 71px 240px auto 240px;
+    grid-template-rows: 46px auto 52px;
+    grid-template-areas:
+      "SL SN CI CI"
+      "SL CL CD UL"
+      "SL UI CD UL"
+    ;
+  }
 </style>
