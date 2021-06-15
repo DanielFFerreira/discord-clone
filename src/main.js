4ace26bcbc1a1ee94c6c1b293065e0d7df3a5c26
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-15 18:13:02 -0300

    Importing global style components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
+
+// Importing the components
 import App from "./App.vue";
 import Layout from "./Components/Layout";
+
+import "./styles/main.scss";
 
 new Vue({
   el: "#app",
