e9192adfe5edeb9cb9eb883940a5de58cc415ca5
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-20 19:05:50 -0300

    Starting structure and styling serverlist component

---
+++ src/Components/ServerList/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <ServerButton />
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    grid-area: SL;
+    flex-direction: column;
+    align-items: center;
+    background: var(--tertiary);
+    padding: 11px 0;
+    max-height: 100vh;
+  }
+</style>
