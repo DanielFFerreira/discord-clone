84728cfe347795531f022fa935d79d9440aa7d73
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-21 00:12:38 -0300

    Add component with mentions and scrool style hasNotification and separation

--- src/Components/ServerList/index.vue
+++ src/Components/ServerList/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container">
+    <ServerButton isHome />
+    <div class="separator"></div>
+    <ServerButton :mentions="3" />
     <ServerButton />
+    <ServerButton :mentions="35"  />
+    <ServerButton :mentions="5" hasNotification />
+    <ServerButton :mentions="1" hasNotification />
+    <ServerButton :mentions="8"  />
+    <ServerButton :mentions="10" hasNotification />
+    <ServerButton />
+    <ServerButton />
+    <!-- <ServerButton v-for="servers in 18" v-if="servers.id" /> -->
+    <ServerButton />
+
   </div>
 </template>
 <script>
@@ -20,5 +33,14 @@ export default {
     background: var(--tertiary);
     padding: 11px 0;
     max-height: 100vh;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  .separator {
+    width: 32px;
+    border-bottom: 2px solid var(--quaternary);
+    margin-bottom: 10px;
   }
 </style>
