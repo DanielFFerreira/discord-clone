62d553c47af0a3a9203243d8108cdf97037436bb
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-20 21:10:15 -0300

    Styling the server and adding transition animation and button focus

--- src/Components/ServerList/ServerButton.vue
+++ src/Components/ServerList/ServerButton.vue
@@ -12,6 +12,23 @@ export default {
 
 <style lang="scss" scoped>
   .serverbutton {
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: center;
+    width: 48px;
+    height: 48px;
+    margin-bottom: 10px;
+    background: var(--primary);
+    cursor: pointer;
+    position: relative;
+    border-radius: 50%;
+    transition: 0.2s;
 
+    &.active,
+    &:hover {
+      border-radius: 16px;
+      background: var(--discord);
+    }
   }
 </style>
