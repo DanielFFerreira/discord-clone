096d918e34e54da0c5d35b4e2bd4b4aeda2e1a4f
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-23 09:40:21 -0300

    Adding component to classes and calling ChannelButton and styling global component

--- src/Components/ChannelList/index.vue
+++ src/Components/ChannelList/index.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    <h1>Teste</h1>
+  <div class="channellist">
+    <div class="category">
+      <div>Canais de Texto</div>
+      <div class="category-icon">
+        <AddCategoryIcon size='21' />
+      </div>
+    </div>
+
+    <ChannelButton selected channelName="chat-livre" />
+    <ChannelButton channelName="vue-js" />
+    <ChannelButton channelName="jogos" />
+    <ChannelButton channelName="csgo" />
+    <ChannelButton channelName="react-js" />
+    <ChannelButton channelName="angular-js" />
+    <ChannelButton channelName="javascript" />
+    <ChannelButton channelName="css" />
+    <ChannelButton channelName="html" />
+    <ChannelButton channelName="java" />
+
   </div>
 </template>
 <script>
+import AddCategoryIcon from 'vue-material-design-icons/Plus';
+import ChannelButton from './ChannelButton';
 export default {
-
+  components: {
+    AddCategoryIcon,
+    ChannelButton
+  }
 }
 </script>
 <style scoped lang="scss">
+  .channellist {
+    grid-area: CL;
+    background: var(--secondary);
+    display: flex;
+    flex-direction: column;
 
+    padding: 24px 10px 0 16px;
+  }
+  .category {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 </style>
