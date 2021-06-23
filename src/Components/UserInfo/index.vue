7ba55802f2ff35e3fcd69c8c69cc248ba1273ca1
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-23 17:54:45 -0300

    Starting the userinfo structure

---
+++ src/Components/UserInfo/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="userinfo">
+    <div class=""></div>
+  </div>
+</template>
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
+<style lang="scss">
+  .userinfo {
+    grid-area: UI;
+    display: flex;
+    flex-direction: row;
+  }
+</style>
