e519eb064920649ad568ad8075133c79eb31277d
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-24 08:20:37 -0300

    Add vue-material-design component and styles

--- src/Components/UserInfo/index.vue
+++ src/Components/UserInfo/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="userinfo">
-    <div class=""></div>
+    <div class="profile">
+      <div class="avatar">
+        <img src="https://avatars.githubusercontent.com/u/50724196?v=4" alt="">
+      </div>
+      <div class="userdata">
+        <strong>Daniel Ferreira</strong><span>#2654</span>
+      </div>
+    </div>
+    <div class="actions">
+      <Mic :size="20" />
+      <HeadPhones :size="20" />
+      <Settings :size="20" />
+    </div>
   </div>
 </template>
 <script>
+import Mic from "vue-material-design-icons/Microphone";
+import HeadPhones from "vue-material-design-icons/Headphones";
+import Settings from "vue-material-design-icons/CogOutline";
 export default {
   components: {
-
+    Mic,
+    HeadPhones,
+    Settings
   }
 }
 </script>
@@ -15,5 +32,54 @@ export default {
     grid-area: UI;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background: var(--quaternary);
+    box-shadow: rgba(0, 0, 0, 0.2) 0 2px 0 0;
+  }
+  .profile {
+    display: flex;
+    align-items: center;
+  }
+  .avatar {
+    width: 37px;
+    height: 37px;
+    border-radius: 50%;
+    background: var(--grey);
+
+    img {
+      width: 37px;
+      height: 37px;
+      border-radius: 50%;
+    }
+  }
+  .userdata {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10 px;
+    font-size: 13px;
+
+    strong {
+      margin-left: 10px;
+      display: block;
+      color: var(--white);
+    }
+    span {
+      padding-left: 10px;
+      color: var(--grey);
+    }
+  }
+  .actions {
+    color: var(--grey);
+
+    span {
+      cursor: pointer;
+      transition: all ease-in-out 0.2s;
+
+      &:hover {
+        color: var(--white);
+      }
+    }
   }
 </style>
