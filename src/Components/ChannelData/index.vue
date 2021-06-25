73bd90664e96a1d6f30b921a1500f503ceea037a
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-25 08:28:08 -0300

    Passing properties and importing data names and methods

--- src/Components/ChannelData/index.vue
+++ src/Components/ChannelData/index.vue
@@ -1,28 +1,48 @@
 <template>
   <div class="channeldata">
     <div class="messages">
-      <ChannelMessage>
-        Este é um teste de mensagem!
+      <ChannelMessage
+        :author="generateName()"
+        date="20/05/2025"
+        v-for="messages in 18"
+        :key="messages.id"
+      >Este é um teste de mensagem!</ChannelMessage>
+      <ChannelMessage ref="new" author="Daniel Ferreira" date="20/05/2025" isBot hasMention>
+        <Mention>Daniel Ferreira</Mention>é um teste de mensagem!
       </ChannelMessage>
     </div>
     <div class="inputwrapper">
-      <input type="text" placeholder="Conversar em #chat-livre">
+      <input type="text" placeholder="Conversar em #chat-livre" />
       <div class="icon">
-        <At :size='24' />
+        <At :size="24" />
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import Vue from 'vue';
 import ChannelMessage from './ChannelMessage.vue';
 import At from "vue-material-design-icons/At";
+
+import randomName from "../../data/names";
+
 export default {
   components: {
     ChannelMessage,
     At
+  },
+  methods: {
+    generateName() {
+      return randomName();
+    }
   }
 }
+
+Vue.component("Mention", {
+  template: '<span class="mention"><slot /></span>'
+});
+
 </script>
 <style scoped lang="scss">
   .channeldata {
@@ -39,6 +59,10 @@ export default {
     height: calc(100vh - 46px - 68px);
     max-height: calc(100vh - 46px - 68px);
     overflow-y: scroll;
+
+    .channel-message:first-child {
+      margin-top: 0;
+    }
 
     &::-webkit-scrollbar {
       width: 8px;
