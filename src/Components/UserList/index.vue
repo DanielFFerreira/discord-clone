df160b7a2aadc4fc3e31cb5c2fd3168815d44bd1
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-06-25 23:02:41 -0300

    Starting structure and importing user names

---
+++ src/Components/UserList/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+import UserRow from './UserRow.vue';
+import ramdomName from '../../data/names';
+export default {
+  components: {
+
+  },
+  methods: {
+    generateName() {
+      return randomName();
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
